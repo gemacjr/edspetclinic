5902d5e247b0fbe0d7f1ea024e90518f48b58c23
Ed McCormic <ed@edmccormic.com>
2021-06-24 20:36:41 -0400

    first commit

---
+++ src/main/java/com/swiftbeard/edspetclinic/EdsPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.swiftbeard.edspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdsPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdsPetclinicApplication.class, args);
+    }
+
+}
